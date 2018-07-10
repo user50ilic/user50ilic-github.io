ed4b430e4cb8ffd1d1f017521a7fb9a9ad2351b8
Marko <markoafx@gmail.com>
2018-07-10 10:34:30 +0200

    footer fix, scroll fix

--- script.js
+++ script.js
@@ -2,16 +2,19 @@
 
 $(document).ready(function(){
   $(window).scrollTop($('#pocetna').offset().top);
-  $('.main-p').addClass('animated fadeInLeft')
+  // $('.main-p').addClass('animated fadeInLeft')
+
+  $('.main-p').css({
+  transition:'all 1s ease 0.40s',
+  opacity:1,
+  transform:'translateX(0px)'
+})
+
   $('.fade-left-up').addClass('animated fadeOut')
   $('.fade-right-up').addClass('animated fadeOut')
   $('.fade-left-down').addClass('animated fadeOut')
   $('.fade-right-down').addClass('animated fadeOut')
 })
-
-
-
-
 
 
 
@@ -35,9 +38,6 @@ $(window).scroll(function (event) {
 
 
 
-
-
-
     if(scroll >=150){
       $('.nav-container').slideUp(200);
     }
@@ -45,7 +45,9 @@ $(window).scroll(function (event) {
       $('.nav-container').slideDown(200);
     }
 
-    if(scroll >200){
+
+
+    if(scroll >300){
       $('.fade-left-up').removeClass('animated fadeOut')
       $('.fade-right-up').removeClass('animated fadeOut')
 
@@ -53,7 +55,7 @@ $(window).scroll(function (event) {
       $('.fade-right-up').addClass('animated fadeIn')
       }
 
-    if(scroll >600){
+    if(scroll >700){
 
       $('.fade-left-down').removeClass('animated fadeOut')
       $('.fade-right-down').removeClass('animated fadeOut')
@@ -66,21 +68,16 @@ $(window).scroll(function (event) {
 });
 
 
+// Scroll on click
 
-
-
-// nav scroll
-
-$('#privatni-nav').click(function() {
-    $('html, body').animate({
-        scrollTop: $('.saznaj').offset().top
-    }, 1000);
+$("#privatni-nav").click(function() {
+  $('html,body').animate({ scrollTop: 800 } ,1000);
 });
 
 $('#priprema').click(function() {
     $('html, body').animate({
         scrollTop: $('#pripreme').offset().top
-    }, 1000);
+    }, 1400);
 });
 
 $('.contact').click(function() {
